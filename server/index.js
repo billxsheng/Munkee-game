7bfa506a61078605c9c8d784aa3bc460992f8471
Bill Sheng <billxsheng@gmail.com>
2018-06-05 23:34:23 -0400

    began socket config

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 app.set('view engine', 'hbs');
-
 app.use(express.static(path.join(__dirname, '../views')));
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -19,14 +23,26 @@ app.get('/online/create',(req, res) => {
     res.render('create');
 });
 
+app.post('/online/create/redirect', (req, res) => {
+
+});
+
 app.get('/online/join', (req, res) => {
     res.render('join');
+});
+
+app.post('/online/join/redirect', (req, res) => {
+
 });
 
 app.get('/online/room', (req, res) => {
     res.render('room');
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('User connected.');
+});
+
+server.listen(3000, () => {
     console.log('started');
 })
